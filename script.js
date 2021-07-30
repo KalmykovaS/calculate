44c1f8371f92a783df580be0bfcabca465594b7b
s.kalmykova <s.kalmykova@advance-club.ru>
2021-07-30 16:16:55 +0300

    lesson02

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-// for lessons
+"use strict";
+
+// lesson01
 
 let money;
 let income;
@@ -9,3 +11,23 @@ let period;
 
 alert('Hello world');
 console.log('Hello world');
+
+// lesson02
+
+money = 1000;
+income = 'freelance';
+addExpenses = 'Интернет, Такси, Коммуналка';
+deposit = true;
+mission = 2000;
+period = 6;
+
+console.log(typeof money);
+console.log(typeof income);
+console.log(typeof deposit);
+console.log(addExpenses.length);
+console.log('Период равен ' + period + ' месяцев');
+console.log('Цель заработать ' + mission + ' долларов');
+console.log(addExpenses.toLowerCase().split(', '));
+
+let budgetDay = money / 30;
+console.log(budgetDay);
