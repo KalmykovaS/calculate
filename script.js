f8ced270f939878c53b3276fb0618e4ba7d97a46
Svetlana Kalmykova <79218609932@ya.ru>
2021-08-03 18:20:57 +0300

    some changes in variables

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ let deposit = confirm('Есть ли у вас депозит в банке?');
 const mission = 60000;
 const period = 6;
 let expenses1 = prompt('Введите обязательную статью расходов?');
-let expenses2 = prompt('Введите обязательную статью расходов?');
 let amount1 = +prompt('Во сколько это обойдется?');
+let expenses2 = prompt('Введите обязательную статью расходов?');
 let amount2 = +prompt('Во сколько это обойдется?');
 
 
@@ -30,20 +30,6 @@ console.log(missionMounth);
 
 let budgetDay = budgetMonth / 30;
 console.log(budgetDay);
-
-// switch (budgetDay) {
-//   case budgetDay >= 1200: 
-//     console.log('У вас высокий уровень дохода');
-//     break;
-//   case budgetDay >= 600 || budgetDay < 1200:
-//     console.log('У вас средний уровень дохода');
-//     break;
-//   case budgetDay < 600 || budgetDay >= 0:
-//     console.log('К сожалению у вас уровень дохода ниже среднего');
-//     break;
-//   default:
-//     console.log('Что то пошло не так');
-// }
 
 if (budgetDay >= 1200) {
   console.log('У вас высокий уровень дохода');
